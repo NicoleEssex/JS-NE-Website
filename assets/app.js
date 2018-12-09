129516d37287b22078303c2fd7ab9dfa6b0ea8cc
Nicole Essex <Essex@Nicole-Essexs-MacBook-Pro-2.local>
2018-12-08 22:10:44 -0700

    linked css and script app.js files

---
+++ assets/app.js
@@ -0,0 +1,14 @@
+// retrieve current time to display proper greeting
+function displayGreeting() {
+    let elGreet = document.getElementById("greeting");
+    let today = new Date();
+    let time = today.getHours();
+    if(time <= 11){
+        elGreet.textContent = "Good Morning,";
+    } else if(time >= 12 && time <=16){
+        elGreet.textContent = "Good Afternoon,";
+    } else {
+        elGreet.textContent = "Good Evening,";
+    }      
+}
+displayGreeting();
